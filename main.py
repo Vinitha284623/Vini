87c8de7d300da28a686b9d28e76177a43b802481
Vinitha284623 <144544865+Vinitha284623@users.noreply.github.com>
2023-09-12 12:34:48 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+#implement factorial number 
+num = 7
+factorial = 1
+if num < 0:
+   print("Sorry, factorial does not exist")
+elif num == 0:
+   print("The factorial of 0 is 1")
+else:
+   for i in range(1,num + 1):
+       factorial = factorial*i
+   print("The factorial of",num,"is",factorial)
